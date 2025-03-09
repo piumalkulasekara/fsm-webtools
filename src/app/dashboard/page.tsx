4e600a371b153abf8e06e4c80f707b95451226e8
Piumal Kulasekara <piumal.kulasekara@gmail.com>
2025-03-09 11:59:33 +0100

    Added a landing page and dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+// src/app/dashboard/page.tsx
+
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-900">
+      <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
+        Dashboard
+      </h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-export default function Home() {
-  return (
-    <>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-    </>
-  )
+// src/app/page.tsx
+
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+import LandingPage from "./landing-page";
+
+export default async function Home() {
+  const { userId } = await auth()
+
+  // If a user is logged in, redirect them to the dashboard
+  if (userId) {
+    redirect("/dashboard");
+  }
+
+  // Otherwise, render the landing page
+  return <LandingPage />;
 }
